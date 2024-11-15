23d44f3b716949cb101621233d429fe449f43b55
DESKTOP-UQ5TGGD\LENOVO <hafiz300@outlook.com>
2024-11-15 15:35:44 +0000

    something is rendering, Allahuma-barik

--- car-inventory-portal/src/app/app.routes.ts
+++ car-inventory-portal/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { MainLayoutComponent } from './core/layout/main-layout/main-layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: MainLayoutComponent,
+    children: [
+      // Define child routes here
+    ]
+  }
+];
