5b58aa53c76165a4c4a0eb40f33acc1cc19c2e44
DESKTOP-UQ5TGGD\LENOVO <hafiz300@outlook.com>
2024-11-17 14:53:07 +0000

    building basic frontend in angular

--- car-inventory-portal/src/app/app.component.ts
+++ car-inventory-portal/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MainLayoutComponent } from './core/layout/main-layout/main-layout.component';
+import { MatIconRegistry } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
@@ -9,6 +10,13 @@ import { MainLayoutComponent } from './core/layout/main-layout/main-layout.compo
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
+
 export class AppComponent {
+  constructor(
+    private matIconRegistry: MatIconRegistry
+  ) {
+    this.matIconRegistry.setDefaultFontSetClass('material-icons');
+  }
+
   title = 'car-inventory-portal';
 }
